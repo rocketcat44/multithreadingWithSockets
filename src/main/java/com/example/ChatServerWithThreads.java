52dea4346b61863adbc68daa680ced438b355497
rocketcat44 <blixinran@gmail.com>
2026-03-26 00:47:02 +0000

    I deleted my first codespace because I forgot to commit and push and I was trying to make space.

--- src/main/java/com/example/ChatServerWithThreads.java
+++ src/main/java/com/example/ChatServerWithThreads.java
@@ -2,65 +2,100 @@ package com.example;
 
 import java.net.*;
 import java.io.*;
-import java.util.Date;
+import java.util.ArrayList;
 
 /**
  * This program is a server that takes connection requests on
  * the port specified by the constant LISTENING_PORT.  When a
- * connection is opened, the program should allow the client to send it messages. The messages should then 
+ * connection is opened, the program should allow the client to send it messages. The messages should then
  * become visible to all other clients.  The program will continue to receive
  * and process connections until it is killed (by a CONTROL-C,
- * for example). 
- * 
+ * for example).
+ *
  * This version of the program creates a new thread for
  * every connection request.
  */
 public class ChatServerWithThreads {
 
-    public static final int LISTENING_PORT = 9876;
+    public static final int LISTENING_PORT = 6081;
 
     public static void main(String[] args) {
 
-        ServerSocket listener;  // Listens for incoming connections.
-        Socket connection;      // For communication with the connecting program.
-
-        /* Accept and process connections forever, or until some error occurs. */
+        ServerSocket listener;  
+        Socket connection;      
 
         try {
             listener = new ServerSocket(LISTENING_PORT);
             System.out.println("Listening on port " + LISTENING_PORT);
             while (true) {
-                  // Accept next connection request and handle it.
+                connection = listener.accept();
+            
+                ConnectionHandler h = new ConnectionHandler(connection);
+                h.start();
+                 
             }
         }
         catch (Exception e) {
-            System.out.println("Sorry, the server has shut down.");
-            System.out.println("Error:  " + e);
+            System.out.println("server shut down");
+            System.out.println("error:  " + e);
             return;
         }
 
-    }  // end main()
+    }
 
-
-    /**
-     *  Defines a thread that handles the connection with one
-     *  client.
-     */
     private static class ConnectionHandler extends Thread {
+        private static volatile ArrayList<ConnectionHandler> handlers = new ArrayList<ConnectionHandler>(); 
         Socket client;
+        ObjectOutputStream oos;
+        ObjectInputStream ois;
         ConnectionHandler(Socket socket) {
             client = socket;
+            try{
+            oos = new ObjectOutputStream(client.getOutputStream());
+            ois = new ObjectInputStream(client.getInputStream());
+
+            synchronized (handlers) {
+            handlers.add(this);
+            }}
+            catch(Exception e){}
+           
         }
         public void run() {
             String clientAddress = client.getInetAddress().toString();
             while(true) {
-	            try {
-	            	//your code to send messages goes here.
-	            }
-	            catch (Exception e){
-	                System.out.println("Error on connection with: " 
-	                        + clientAddress + ": " + e);
-	            }
+                try {
+                    String message = (String)ois.readObject();
+                    if(!message.equals("disconnect")){
+                        System.out.println(message);
+                        
+                        synchronized (handlers) {
+                            for (ConnectionHandler h : handlers){
+                                try{
+                                    h.oos.writeObject(message);
+                                    h.oos.flush();
+                                }
+                                catch (IOException e){
+                                }
+                            }
+                        }
+                    }
+                    else{
+                        System.out.println("closing connection");
+                        break;
+                       
+                    }
+                }
+                catch(EOFException e){
+                    System.out.println("client disconnected: " + clientAddress);
+                    synchronized (handlers) {
+                    handlers.remove(this);
+                    }
+                    break;
+                }
+                catch (Exception e){
+                    System.out.println("Error on connection with: "
+                            + clientAddress + ": " + e);
+                }
             }
         }
     }
--- src/main/java/com/example/SocketClientExample.java
+++ src/main/java/com/example/SocketClientExample.java
@@ -5,43 +5,89 @@ import java.io.ObjectOutputStream;
 import java.net.InetAddress;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import javax.swing.JFrame;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import java.awt.BorderLayout;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
 
 public class SocketClientExample {
-	
-	
-	/*
-	 * Modify this example so that it opens a dialogue window using java swing, 
-	 * takes in a user message and sends it
-	 * to the server. The server should output the message back to all connected clients
-	 * (you should see your own message pop up in your client as well when you send it!).
-	 *  We will build on this project in the future to make a full fledged server based game,
-	 *  so make sure you can read your code later! Use good programming practices.
-	 *  ****HINT**** you may wish to have a thread be in charge of sending information 
-	 *  and another thread in charge of receiving information.
-	*/
+   
+   
+    /*
+     * Modify this example so that it opens a dialogue window using java swing,
+     * takes in a user message and sends it
+     * to the server. The server should output the message back to all connected clients
+     * (you should see your own message pop up in your client as well when you send it!).
+     *  We will build on this project in the future to make a full fledged server based game,
+     *  so make sure you can read your code later! Use good programming practices.
+     *  ****HINT**** you may wish to have a thread be in charge of sending information
+     *  and another thread in charge of receiving information.
+    */
     public static void main(String[] args) throws UnknownHostException, IOException, ClassNotFoundException, InterruptedException{
-        //get the localhost IP address, if server is running on some other IP, you need to use that
         InetAddress host = InetAddress.getLocalHost();
-        Socket socket = null;
-        ObjectOutputStream oos = null;
-        ObjectInputStream ois = null;
-        for(int i=0; i<5;i++){
-            //establish socket connection to server
-            socket = new Socket(host.getHostName(), 9876);
-            //write to socket using ObjectOutputStream
-            oos = new ObjectOutputStream(socket.getOutputStream());
-            System.out.println("Sending request to Socket Server");
-            if(i==4)oos.writeObject("exit");
-            else oos.writeObject(""+i);
-            //read the server response message
-            ois = new ObjectInputStream(socket.getInputStream());
-            String message = (String) ois.readObject();
-            System.out.println("Message: " + message);
-            //close resources
-            ois.close();
-            oos.close();
-            Thread.sleep(100);
-        }
+        Socket socket = new Socket(host.getHostName(), 6081); 
+        ObjectOutputStream   oos = new ObjectOutputStream(socket.getOutputStream());
+        ObjectInputStream ois = new ObjectInputStream(socket.getInputStream());
+
+        JFrame frame = new JFrame("Client Chat");
+            JTextArea serverText = new JTextArea();
+            JTextField clientText = new JTextField();
+
+            serverText.setEditable(false);
+            clientText.setEditable(true);
+
+
+            frame.setLayout(new BorderLayout());
+            frame.setSize(400,400);
+            frame.add(serverText, BorderLayout.CENTER);
+            frame.add(clientText, BorderLayout.SOUTH);
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.setVisible(true);
+
+            frame.addWindowListener(new WindowAdapter() {
+                @Override
+                public void windowClosing(WindowEvent e) {
+                    try {
+                        oos.writeObject("disconnect");
+                        oos.flush();
+                    } catch (IOException ex) {
+                    }
+                }
+            });
+
+
+
+            Thread t = new Thread(() -> {
+                boolean running = true;
+                while (running) {
+                    try {
+                        String message = (String)ois.readObject();
+                        serverText.append(message + "\n");
+                    } catch (Exception e) {
+                        serverText.append("disconnected\n");
+                        running = false;
+                    }
+                }
+            });
+            t.start();
+
+
+
+
+
+            clientText.addActionListener(e -> {
+            String input = clientText.getText();
+            clientText.setText("");
+
+try{
+oos.writeObject(input);
+            oos.flush();
+}
+catch (IOException etwo){
+}   
+        });
     }
 }
